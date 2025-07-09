2c614470f0b88bf71c624e837a52a4822d6d55f5
takascemberi <takascemberi@gmail.com>
2025-07-09 10:14:37 +0000

    firebaseConfig.ts oluşturuldu

---
+++ okeycini/firebase/firebaseConfig.ts
@@ -0,0 +1,15 @@
+// firebase/firebaseConfig.ts
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGAzDRjQgRL5fq4y-kp96r_rpDYwpyGCI",
+  authDomain: "okeycinifal.firebaseapp.com",
+  projectId: "okeycinifal",
+  storageBucket: "okeycinifal.firebasestorage.app",
+  messagingSenderId: "998696978502",
+  appId: "1:998696978502:web:0f02afe967629ae69d6247"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
